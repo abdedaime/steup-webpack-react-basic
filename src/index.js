eef3ad8d52fde9fd8614a5e64e1536e288ec3b3c
hicham abdedaime <hicham.abdedaime@adria-bt.com>
2019-01-29 22:24:50 +0100

    feat:first commit initial project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const title = <h1> My Minimal React Webpack Babel Setup </h1>;
+
+
+ReactDOM.render(
+  title,
+  document.getElementById('app')
+)
+
+
+
